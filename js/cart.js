3599fd6442515845eaf17f34d2a1d08cdb347d34
Siriskarsvag <siri.skarsvaag@hotmail.com>
2026-05-06 18:24:33 +0200

    Cart page built
    - Built HTML
    - Rendered cart from localStorage
    - Built functon for getting cart total
    - styled page according to design
    - created a checkout button(non functionable)

--- js/cart.js
+++ js/cart.js
@@ -19,9 +19,62 @@ function addToCart(product) {
             title: product.title,
             price: product.discountedPrice < product.price ? product.discountedPrice : product.price,
             quantity: 1,
-            image: product.image.url
+            image: product.image
         });
     }
 
     saveCartItems(cartItems);
 }
+
+function getCartTotal() {
+    return getCartItems().reduce((total, item) => total + item.price * item.quantity, 0);
+}
+
+function renderCartTotal() {
+    const totalElement = document.getElementById("cart-total");
+    if (!totalElement) return;
+
+    const cartItems = getCartItems();
+    const total = getCartTotal(cartItems);
+
+    totalElement.textContent = `Total: ${total.toFixed(2)},-`;
+}
+
+function renderCart() {
+    const itemTable = document.getElementById("cart-items-table");
+    if (!itemTable) return;
+
+    const cartItems = getCartItems();
+    itemTable.innerHTML = "Loading cart items...";
+    itemTable.innerHTML = "";
+    
+    if (cartItems.length === 0) {
+        itemTable.innerHTML = "Your cart is empty.";
+        renderCartTotal();
+        return;
+    }
+
+    cartItems.forEach((item) => {
+        itemTable.innerHTML += `
+            <tr>
+                <td  class="cart-item-image">
+                    <img src="${item.image}">
+                </td>
+                <td class="cart-item-title">
+                    <h6>${item.title}</h6>
+                </td>
+                <td class="cart-item-quantity">
+                    <button class="decrease-quantity">-</button><h6>${item.quantity}</h6><button class="increase-quantity">+</button><img src="assets/waste-bin_7951984.png" alt="Remove item" class="remove-item">
+                </td>
+                <td class="cart-item-price">
+                    <h6>${item.price},-</h6>
+                </td>
+            </tr>
+
+        `;
+    });
+
+    renderCartTotal();
+}
+
+renderCart();
